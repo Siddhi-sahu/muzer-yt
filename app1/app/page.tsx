4e3fb0762a04cbb3046816fc82f0780893630c2f
Siddhi Sahu <himanisahu739@gmail.com>
2024-10-22 18:19:36 +0530

    commit

--- app1/app/page.tsx
+++ app1/app/page.tsx
@@ -3,6 +3,7 @@
 
 
 export default function Home() {
+  console.log(process.env.GOOGLE_CLIENT_ID)
   return (
     <div> hii there
 
---
+++ app1/next.config.mjs
@@ -0,0 +1,10 @@
+// next.config.mjs
+import { NextConfig } from "next";
+
+/** @type {NextConfig} */
+const nextConfig = {
+  // your Next.js configuration options here
+};
+
+export default nextConfig;
+
